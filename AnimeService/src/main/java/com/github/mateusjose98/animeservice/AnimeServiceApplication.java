a06f83002a4e805faf89f6b9a69e56839e62cd6b
jm <josemateus.ufma@gmail.com>
2024-12-13 18:03:25 -0300

    initial commit

---
+++ AnimeService/src/main/java/com/github/mateusjose98/animeservice/AnimeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.mateusjose98.animeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeServiceApplication.class, args);
+    }
+
+}
